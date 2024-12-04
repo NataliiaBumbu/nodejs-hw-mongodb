8e446cfe4cb435facb6191ea427bdf117c13aea8
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2024-12-04 13:18:37 +0200

    HW

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use('/contacts', contactsRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 2020;
+const PORT = process.env.PORT || 3001;
 
 async function startServer() {
   try {
