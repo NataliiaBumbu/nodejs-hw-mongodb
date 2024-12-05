e7375be04de4260289218b05d766647b43284537
NataliiaBumbu <130250304+NataliiaBumbu@users.noreply.github.com>
2024-12-05 11:54:40 +0200

    Hw

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use('/contacts', contactsRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 6070;
 
 async function startServer() {
   try {
--- src/utils/contactSchemas.js
+++ src/utils/contactSchemas.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().required(),
-  contactType: Joi.string().valid('friend', 'family', 'work').required(),
+  contactType: Joi.string().required(), 
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
 });
@@ -11,7 +11,7 @@ const createContactSchema = Joi.object({
 const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   phoneNumber: Joi.string(),
-  contactType: Joi.string().valid('friend', 'family', 'work'),
+  contactType: Joi.string(), 
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
 }).min(1);
